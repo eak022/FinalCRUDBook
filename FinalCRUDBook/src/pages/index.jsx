0a52ac914be1f0f36d9813d57d0e41725b2565db
eak022 <121035260+eak022@users.noreply.github.com>
2023-12-18 22:32:57 +0700

    Delete FinalCRUDBook directory

--- FinalCRUDBook/src/pages/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as AddBook } from "./AddBook";
-export { default as EditBook } from "./EditBook";
-export { default as BookDetail } from "./BookDetail";
-export { default as BookList } from "./BookList";
